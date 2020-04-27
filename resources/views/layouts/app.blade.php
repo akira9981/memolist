4d20ca4e076862e3e65c00d5d93d30db03fb8c34
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-04-28 03:38:35 +0900

    fix lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,11 +47,11 @@
       <li>
         <ul class="header_menu_inner">
           <li>
-              <a class="nav-link listNew" href="{{ route('new') }}">add list</a>　　
+              <a class="nav-link listNew" href="{{ route('new') }}">Add list</a>　　
           </li>
           <li>
             <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-              Logout
+              Sign out
             </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
               {{ csrf_field() }}
