b0248f2700c409592c78505e63b74d6ede6e9dc4
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-04-28 02:26:14 +0900

    list file fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,7 @@
     <link href="{{ asset('css/card_edit.css') }}" rel="stylesheet">
     <link href="{{ asset('css/card_new.css') }}" rel="stylesheet">
     <link href="{{ asset('css/card_show.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/list_new.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/list_edit.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/list.css') }}" rel="stylesheet">
     <link href="{{ asset('css/hamburger.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
